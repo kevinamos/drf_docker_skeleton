8b6ef31745b7873a3892978b1a32fd2f90c76eba
kevinamos <kevinongango@gmail.com>
2019-06-28 09:35:52 +0300

    add custom user model and normalize email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         email = 'testkev@gmail.com'
-        password = 'pass4321'
+        password = 'Rmbki78@9'
         user = get_user_model().objects.create(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
